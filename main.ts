a2493a943241dfece500f3f904d313c9537e2b4c
makerchanges <83437768+makerchanges@users.noreply.github.com>
2021-04-30 10:58:26 +0900

    움직이는 하트

--- main.ts
+++ main.ts
@@ -8,6 +8,9 @@ input.onButtonPressed(Button.B, function () {
     basic.showArrow(ArrowNames.East)
 })
 basic.showIcon(IconNames.Heart)
+basic.pause(1000)
+basic.showIcon(IconNames.SmallHeart)
+basic.pause(1000)
 basic.forever(function () {
 	
 })
